c345a533e228cabc5ba5cde5850dc393af6585b0
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-04-08 02:34:36 -0700

    setting up if statements to work with older versions of starry and trying sph coefficients of 1000

--- lightcurves_sh_starry.py
+++ lightcurves_sh_starry.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import pickle
 import starry
-starry.config.lazy = False
-starry.config.quiet = True
+if starry.__version__ >= '1.0':
+    starry.config.lazy = False
+    starry.config.quiet = True
 from pca_eig import princomp
 import pdb
 
@@ -13,22 +14,51 @@ import pdb
 #      return np.amin(x),np.amax(x),np.nanmin(x),np.nanmax(x)
 
 
+def make_lc(star,planet,time):
+	if starry.__version__ >='1.0':
+		system = starry.System(star, planet)
+		flux_star, lightcurve = system.flux(time, total=False)
+	else:
+		# Create starry system with star and planet
+		system = starry.kepler.System(star, planet)
+		system.compute(time)
+		lightcurve = system.lightcurve / system.lightcurve[0]
+	return lightcurve
+
 def sh_lcs(t0=0,per=2.21857567,inc=85.71,ecc=0.0,w=90,rp=0.155313,a=8.863,ntimes=500,degree=3):
 	# initialize star and planet parameters
-	star = starry.Primary(starry.Map(ydeg=0,udeg=0,amp=1.0), m=1.0, r=1.0, prot=1.0)
+	if starry.__version__ >= '1.0':
+		star = starry.Primary(starry.Map(ydeg=0,udeg=0,amp=1.0), m=1.0, r=1.0, prot=1.0)
 
-	planet = starry.kepler.Secondary(
-	    starry.Map(ydeg=int(degree-1), amp=1.),  # surface map, amp is luminosity relative to star (normalization is Y00)
-	    #m=0.0009543,  # mass in solar masses
-	    r=rp,  # radius in solar radii
-	    porb=per,  # orbital period in days
-	    prot=per,  # rotation period in days
-	    inc=inc,   # inclination of orbit in degrees
-	    a=a,	#a/rstar
-	    ecc=ecc,  # eccentricity
-	    w=w,  # longitude of pericenter in degrees
-	    t0=t0,  # time of transit in days
-	)
+		planet = starry.kepler.Secondary(
+		    starry.Map(ydeg=int(degree-1), amp=1.),  # surface map, amp is luminosity relative to star (normalization is Y00)
+			#m=0.0009543,  # mass in solar masses
+			r=rp,  # radius in solar radii
+			porb=per,  # orbital period in days
+			prot=per,  # rotation period in days
+			inc=inc,   # inclination of orbit in degrees
+			a=a,	#a/rstar
+			ecc=ecc,  # eccentricity
+			w=w,  # longitude of pericenter in degrees
+			t0=t0,  # time of transit in days
+		)
+	else:
+		## Star
+		star = starry.kepler.Primary()#nwav=Nlamlo)
+		# Create starry planet
+		planet = starry.kepler.Secondary(lmax=int(degree-1))#,nwav=Nlamlo)
+		
+		# HD189 parameters
+		planet.r = rp
+		L = 0.00344
+		planet.L = L #* np.ones(Nlamlo)
+		planet.inc = inc
+		planet.a = a
+		planet.prot = per
+		planet.porb = per
+		planet.tref = 0.0#-per/2.0
+		planet.ecc = ecc
+		planet.w = w
 
 	# arrays of time and fluxes for secondary eclipse curves:
 	if np.size(ntimes) == 1:
@@ -46,8 +76,9 @@ def sh_lcs(t0=0,per=2.21857567,inc=85.71,ecc=0.0,w=90,rp=0.155313,a=8.863,ntimes
 	#pdb.set_trace()
 	shi=0
 	#planet.map[0,0]=1.0
-	system = starry.System(star, planet)
-	flux_star, SHcurves[shi,:] = system.flux(time, total=False)
+	
+	SHcurves[shi,:] = make_lc(star,planet,time)
+	
 	shi+=1
 	#planet.map[0,0]=0.0
 	for l in range(1,degree):
@@ -55,25 +86,36 @@ def sh_lcs(t0=0,per=2.21857567,inc=85.71,ecc=0.0,w=90,rp=0.155313,a=8.863,ntimes
 	        #print(shi,l,m)
 	                
 	        # calculate negative version of SH component:
-			planet.map[l,m]=-1.0
-			system = starry.System(star, planet)
-			flux_star, SHcurves[shi,:] = system.flux(time, total=False)
+			if starry.__version__ >= '1.0':
+				planet.map[l,m]=-1.0
+			else:
+				planet[l,m]=-1000.0
+			
+			SHcurves[shi,:] = make_lc(star,planet,time)
+			
 			#flux_star, temp = system.flux(time, total=False)
 			#SHcurves[shi,:] = temp-SHcurves[0,:] #subtract off the zeroth degree thing
 			shi+=1
 
 	        # calculate positive version of SH component:
-			planet.map[l,m]=1.0
-			system = starry.System(star, planet)
-			flux_star, SHcurves[shi,:] = system.flux(time, total=False)
+			if starry.__version__ >= '1.0':
+				planet.map[l,m]=1.0
+			else:
+				planet[l,m] = 1000.0
+			
+			SHcurves[shi,:] = make_lc(star,planet,time)
 			#flux_star, temp = system.flux(time, total=False)
 			#SHcurves[shi,:] = temp-SHcurves[0,:] #subtract off the zeroth degree thing
 			shi+=1
 
 	        # reset coefficient to zero
-			planet.map[l,m]=0
-	        #pdb.set_trace()
-
+			if starry.__version__ >= '1.0':
+				planet.map[l,m]=0
+			else:
+				planet.reset()
+			
+			
+	#pdb.set_trace()
 	return SHcurves,time
 
 
